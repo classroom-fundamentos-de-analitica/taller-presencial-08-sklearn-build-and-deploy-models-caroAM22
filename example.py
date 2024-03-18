137ee097c3951a0c29cdf2af1274a69f7cdf5b78
caroAM22 <caalvarezmu@unal.edu.co>
2024-03-18 10:16:45 -0500

    Update taller 8

---
+++ example.py
@@ -0,0 +1,8 @@
+import pickle
+import pandas as pd
+
+with open("house_predictor.pickle", "rb") as file:
+    loaded_model = pickle.load(file)
+
+print(loaded_model.coef_)
+print(loaded_model.intercept_)
